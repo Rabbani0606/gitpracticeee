10045b9e47dc1dc78a7233449d0739e2a75f4e03
rabbani <rabbani41770@gmail.com>
2025-04-07 20:09:58 +0530

    i have pushed app.py file

---
+++ app.py
@@ -0,0 +1,11 @@
+# Simple Python program
+
+# Print a message
+print("Hello, World!")
+
+# Ask for user's name
+name = input("What's your name? ")
+
+# Greet the user
+print(f"Nice to meet you, {name}!")
+
